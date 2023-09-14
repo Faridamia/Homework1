dbf7cfb0ddc3ccc8424b756c6e6b1820054f3180
Your Name <youremail@yourdomain.com>
2023-09-14 17:35:14 +0600

    first commit

---
+++ bin/git_homework1.dart
@@ -0,0 +1,5 @@
+import 'package:git_homework1/git_homework1.dart' as git_homework1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_homework1.calculate()}!');
+}
---
+++ test/git_homework1_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_homework1/git_homework1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
