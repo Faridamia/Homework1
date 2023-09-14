14f70361b7e578b84f433f089dc83cd35299d051
Your Name <youremail@yourdomain.com>
2023-09-15 00:21:33 +0600

    second commit

--- bin/git_homework1.dart
+++ bin/git_homework1.dart
@@ -1,5 +1,39 @@
-import 'package:git_homework1/git_homework1.dart' as git_homework1;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${git_homework1.calculate()}!');
+void main() {
+  // Задача 1
+
+//print('Введите первое число');
+//double a = double.parse(stdin.readLineSync()!);
+//print('Введите второе число');
+//double b = double.parse(stdin.readLineSync()!);
+//print('$a + $b-Сложение\n$a - $b-Вычитание\n$a * $b -Умножение\n$a / $b-Деление');
+
+// Задача 2
+
+// print('Введите число');
+//int a = int.parse(stdin.readLineSync()!);
+//print(a.isEven);
+
+// Задача 3
+
+//print('Введите число');
+//int a = int.parse(stdin.readLineSync()!);
+//print('$a * 1 = ${a * 1}\n$a * 2 = ${a * 2}\n$a * 3 = ${a * 3}\n$a * 4 = ${a * 4}\n$a * 5 = ${a * 5}\n$a * 6 = ${a * 6}\n$a * 7 = ${a * 7}\n$a * 8 = ${a * 8}\n$a * 9 = ${a * 9}\n$a * 10 = ${a * 10}');
+
+// Задача 4
+
+//print('Введите градусы в Целсиях');
+//double a = double.parse(stdin.readLineSync()!);
+//print('$a в Целсиях = ${(a * 9 / 5) + 32} в Фаренгейтах');
+
+// Задача 5
+
+  // print('Введите сумму');
+  //double summ = double.parse(stdin.readLineSync()!);
+  //print('Введите название валюты');
+  //String valuta = stdin.readLineSync()!;
+  //print('Введите стоимость валюты');
+  //double price = double.parse(stdin.readLineSync()!);
+  //print('$summ $valuta = ${price * summ} сом');
 }
